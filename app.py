e5850009e4c9f0603333419e09ce729936b0ce45
Gowtham-Ambadi <ambadigowtham@gmail.com>
2025-12-19 20:13:42 +0530

    restored

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, YANCers</h1><p>Welcome to the YANC Cohort Meet!!</p>'
+    return '<h1>Hello, World!</h1><p>Welcome to the World!</p>'
 
 @app.route('/health')
 def health():
